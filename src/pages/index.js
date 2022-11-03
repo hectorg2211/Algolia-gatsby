b6e4b274d2d690041dab7b42ebfa510750228294
hectorg2211 <hector032716@gmail.com>
2022-11-03 09:35:02 -0700

    ⭐Add working refinement link scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,36 @@
-import * as React from 'react'
+import React, { useLayoutEffect, useState, useCallback } from 'react'
 import {
   RefinementList,
   InfiniteHits,
   Highlight,
   InstantSearch,
+  useInstantSearch,
 } from 'react-instantsearch-hooks-web'
 import algoliasearch from 'algoliasearch/lite'
 import { Link } from 'gatsby'
 
 const searchClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76')
 
-const IndexPage = () => {
+const IndexPage = ({ location }) => {
+  const [div, setDiv] = useState(null)
+
+  const ref = useCallback((node) => {
+    if (node !== null) {
+      setDiv(node)
+    }
+  }, [])
+
   return (
     <InstantSearch searchClient={searchClient} indexName='instant_search' routing={true}>
       <RefinementList attribute='brand' hitComponent={ProductHit} />
+      <RestoreScrollPosition div={div} />
       <InfiniteHits hitComponent={ProductHit} />
+      <div style={{ margin: 'auto', width: '500px', height: '500px', background: 'red' }}></div>
+      <div style={{ margin: 'auto', width: '500px', height: '500px', background: 'blue' }}></div>
+      <div
+        ref={ref}
+        style={{ margin: 'auto', width: '500px', height: '500px', background: 'green' }}
+        id='green'></div>
     </InstantSearch>
   )
 }
@@ -31,4 +47,21 @@ function ProductHit({ hit }) {
       <Highlight attribute='name' hit={hit} />
     </Link>
   )
+}
+
+function RestoreScrollPosition({ div }) {
+  const { results } = useInstantSearch()
+
+  useLayoutEffect(() => {
+    // Detect if the current page url contains a search query
+    const searchQuery = window.location.search
+    if (searchQuery && Boolean(div))
+      window?.scrollTo({
+        top: div.getBoundingClientRect().top,
+        behavior: 'smooth',
+      })
+    console.log(searchQuery, div?.getBoundingClientRect().top)
+  }, [window.location, results.nbHits])
+
+  return null
 }
